cdd57284a81b8fa10bdc207da174195680ab8d58
yousufkhan13 <khanzzyyy@gmail.com>
2024-01-27 18:18:30 +0500

    answer3

---
+++ question3.dart
@@ -0,0 +1,20 @@
+void main() {
+  List days = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday'
+  ];
+  days.removeLast();
+  days.removeLast();
+  days.removeLast();
+  days.removeLast();
+  days.removeLast();
+  days.removeLast();
+  days.removeLast();
+
+  print(days);
+}
