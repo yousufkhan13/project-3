bd70ebebb9dc9fbba8fffed588ef7f263f6a3f6b
yousufkhan13 <khanzzyyy@gmail.com>
2024-01-27 18:03:48 +0500

    answer2

---
+++ question2.dart
@@ -0,0 +1,15 @@
+void main() {
+  List<String> days = [];
+
+  days.addAll([
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday'
+  ]);
+
+  print(days);
+}
