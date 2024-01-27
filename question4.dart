d781ff04796400ae5e2ee6aa36b70bebd8b76b0d
yousufkhan13 <khanzzyyy@gmail.com>
2024-01-27 18:29:57 +0500

    answer4

---
+++ question4.dart
@@ -0,0 +1,8 @@
+void main() {
+  List numbers = [23, 4, 5, 7, 23, 9, 87, 67];
+
+  numbers.sort();
+
+  print("smallest value in the list : ${numbers.first}");
+  print("smallest value in the list : ${numbers.last}");
+}
