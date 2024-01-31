cf7c54d51254e20d4dd035e0cbd46d613563a5e9
yousufkhan13 <khanzzyyy@gmail.com>
2024-01-31 17:12:51 +0500

    answer22

---
+++ question22.dart
@@ -0,0 +1,8 @@
+void main() {
+  Map cart = {'toys': 25, 'apple': 3, 'fruit': 7, 'mango': 8};
+  if (cart['apple'] > 0) {
+    print('product found');
+  } else {
+    print('product not found');
+  }
+}
